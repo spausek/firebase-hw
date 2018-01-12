01c19bf6aff86bd870efa517cc8b46b2e5ed265a
spausek <31174081+spausek@users.noreply.github.com>
2018-01-12 14:39:25 -0800

    Added to java.js
    Added funtions to create an object containing all the input data and pushing it to firebase.

--- assets/java.js
+++ assets/java.js
@@ -15,13 +15,49 @@
     event.preventDefault();
     var trainName = $('#train-name').val().trim();
     var destination = $('#destination').val().trim();
-    var trainTime = $('#train-time').val().trim();
+    var trainTime = moment($('#train-time').val().trim(),'HH:mm').format('x');
     var frequency = $('#frequency').val().trim();
+    //console.log(trainName);
+    //console.log(destination);
+    //console.log(trainTime);
+    //console.log(frequency);
+
+    var newTrain = {
+      name: trainName,
+      destination: destination,
+      time: trainTime,
+      frequency: frequency,
+
+
+    };
+
+    database.ref().push(newTrain);
+
+    console.log(newTrain);
+    alert('Train Added! Choo Choo!')
+    $('#train-name').val('');
+    $('#destination').val('');
+    $('#train-time').val('');
+    $('#frequency').val('');
+    
+
+
+
+
+  });
+
+  database.ref().on('child_added', function(childSnapshot, prevChildKey) {
+    console.log(childSnapshot.val());
+
+    //var addData = childSnapshot.val();
+    var trainName = childSnapshot.val().name;
+    var destination = addData.destination;
+    var trainTime = addData.time;
+    var frequency = addData.frequency;
+
     console.log(trainName);
     console.log(destination);
     console.log(trainTime);
     console.log(frequency);
-    $('#train-name').val('');
-
 
   });
